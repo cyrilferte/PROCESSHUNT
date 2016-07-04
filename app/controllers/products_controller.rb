d3b15f48e494a26bb2a1688b5129539402614003
cyril ferte <cyril@fertec.fr>
2016-07-04 00:58:02 +0000

    form et add in db

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,6 +13,26 @@ class ProductsController < ApplicationController
 
   end
 
+  def new
+    @product = Product.new
+  end
+  def create
+@product = Product.new(product_params)
+   if @product.save
+    redirect_to products_path
+   else 
+     render :new
+   end
+  end
+  def update
+  end
+  def destroy
+  end
 
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :url)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,17 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get "/team" => "pages#team"
   get "/contact" => "pages#join_us"
-   get "/products" => "products#index"
-   get "/products/:id" => "products#show"
+
+  resources :products
+
+  #  get "/products" => "products#index"
+#   get "/products/:id" => "products#show"
+ # #create
+  #get "/products/new" => "products#new"
+  #post "/products" => "products#ncreate"
+  ##update
+  #  get "/products/:id/edit" => "products#edit"
+  # patch "/products/:id" => "products#update"
+  ##delete
+  #delete "products/:id" => "products#destroy"
 end
