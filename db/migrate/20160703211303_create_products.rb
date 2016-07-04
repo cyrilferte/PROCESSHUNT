52d25da91321b5ba7a2bd125963b3b8f58c61167
cyril ferte <cyril@fertec.fr>
2016-07-04 23:55:45 +0000

    fixed bug heroku1

--- db/migrate/20160703211303_create_products.rb
+++ db/migrate/20160703211303_create_products.rb
@@ -3,8 +3,7 @@ class CreateProducts < ActiveRecord::Migration[5.0]
     create_table :products do |t|
       t.string :name
       t.string :url
-      t.string :category
-      t.string :description
+     
 
       t.timestamps
     end
